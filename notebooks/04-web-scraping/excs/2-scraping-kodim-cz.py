c697dcceeb048df9ebfd142c50d3a5bb79adeda2
AnetaPopelova <aneta.popelova@gmail.com>
2024-05-15 21:08:00 +0200

    add file

--- notebooks/04-web-scraping/excs/2-scraping-kodim-cz.py
+++ notebooks/04-web-scraping/excs/2-scraping-kodim-cz.py
@@ -1,36 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Vytvoření session
-session = requests.Session()
-
 # Načtení webové stránky
 url = "https://kodim.cz/czechitas"
-response = session.get(url)
+response = requests.get(url)
 
 # Vytvoření instance BeautifulSoup
 soup = BeautifulSoup(response.text, 'html.parser')
 
-
 # Hledání elementů obsahujících názvy kurzů
 # Předpokládá se, že názvy kurzů jsou ve elementech se specifickou třídou
-sections = soup.select(".styles_courseCard__EMR1m")
-
+course_titles = soup.select(".styles_courseCard__EMR1m")
 links = []
 # Vypsání názvů sekcí
-for section in sections:
+for course in course_titles:
     # Přidání atributu href (odkaz) každé sekce do seznamu links
-    if 'href' in section.attrs:
-        links.append(section['href'].replace('/czechitas', ''))
+    links.append(course['href'].replace('/czechitas', ''))
 print(links)
 
-# Iterace přes každý odkaz v seznamu links
+# # Iterace přes každý odkaz v seznamu links
 for link in links:
     # Sestavení URL pro každou sekci
     section_url = url + link
-    print(section_url)
-    # Načtení webové stránky sekce
-    section_response = session.get(section_url)
+    section_response = requests.get(section_url)
     # Vytvoření instance BeautifulSoup pro stránku sekce
     section_soup = BeautifulSoup(section_response.text, 'html.parser')
     # Hledání všech h2 elementů, předpokládá se, že tyto obsahují názvy kurzů
@@ -39,5 +31,3 @@ for link in links:
         # Vypsání textu každého h2 elementu, což je název lekce
         print(title.text)
 
-# Uzavření session
-session.close()
